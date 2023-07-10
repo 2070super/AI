5f168ee68cf3c86ab3f8573c31ea6d7676008c65
2070super <1025404072@qq.com>
2023-07-10 11:34:32 +0800

    卷积核

---
+++ 衣服分类卷积神经网络.py
@@ -0,0 +1,29 @@
+import tensorflow as tf
+import pylab
+import  numpy as np
+import matplotlib.pyplot as plt
+from tensorflow import keras
+class mycallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self,epoch,logs={}):
+        if(logs.get('loss')<0.01):
+            print("\n loss is low,so cancelling training")
+            self.model.stop_training=True
+clouth=keras.datasets.fashion_mnist
+(train_images,train_labels),(test_images,test_labels)=clouth.load_data()
+#plt.imshow(train_images[2])
+#pylab.show()
+callbacks=mycallback()
+train_images=train_images/255.0
+test_images=test_images/255.0
+model=keras.Sequential([
+    keras.layers.Conv2D(64,(3,3),activation='relu',input_shape=(28,28,1)),
+    keras.layers.MaxPooling2D(2,2),
+    keras.layers.Conv2D(64, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+    keras.layers.MaxPooling2D(2, 2),
+    keras.layers.Flatten(),
+    keras.layers.Dense(128,activation=tf.nn.relu),
+    keras.layers.Dense(10,activation=tf.nn.softmax)
+])
+model.compile(optimizer='adam',loss='sparse_categorical_crossentropy')
+model.fit(train_images,train_labels,epochs=500,callbacks=[callbacks])
+model.evaluate(test_images,test_labels)
